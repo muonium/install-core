58e0bee6c0827358be0833e1fc5f2f0ff6839377
Super-Baleine <paul.feuvraux@muonium.ch>
2017-05-21 12:56:17 +0200

    put the apache conf in the vars file
    Signed-off-by: Super-Baleine <paul.feuvraux@muonium.ch>

--- debian/jessie.sh
+++ debian/jessie.sh
@@ -4,17 +4,6 @@ install_jessie(){
     mkdir -p /srv/muonium;mkdir -p /srv/muonium/nova;
     git clone https://github.com/muonium/core /srv/muonium/;
     chown -R $apache_USER:$apache_GROUP /srv/muonium;
-    conf_apache='
-    <VirtualHost *:80>
-        DocumentRoot "/srv/muonium/"
-
-        <Directory />
-            Require all granted
-            AllowOverride All
-            Options None
-        </Directory>
-    </VirtualHost>
-    '
 
     echo $conf_apache > /etc/apache2/sites-available/muonium.conf; #put the conf in the vhost
     cd /etc/apache2/sites-available/;a2enmod muonium.conf; #enable the vhost
