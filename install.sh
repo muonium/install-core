8948425f8bfec3c6e64dc8b598c77787ea8cecdd
Super-Baleine <paul.feuvraux@muonium.ch>
2017-05-25 14:54:32 +0200

    wtf was this shit
    Signed-off-by: Super-Baleine <paul.feuvraux@muonium.ch>

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 if [ -e /etc/debian_version ];then
     os=$(cat /etc/os-release|grep "VERSION_ID")
-    if [ $os="VERSION_ID=\"8\"" ];then #debian jessie
+    if [ $os="VERSION_ID=\"8\"" ];then #debian jessie
         source ./debian/jessie.sh; #source the right file
         install_jessie; #call the install function
         echo "##############################
